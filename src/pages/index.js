888ef5b116571195050a8c8049c050277eb4443f
ssarangi17 <sarangi_sanjay@yahoo.com>
2026-02-19 00:01:05 +0530

    Update greeting message in Home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{"Sanjay "}
+      Hello World.{"Sanjay "} Are you having a nice time ? <br>
       <Link href="/about">
         About
       </Link>
