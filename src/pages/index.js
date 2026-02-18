c3421b095fa3949b7242c2da8aafa7281556d881
ssarangi17 <sarangi_sanjay@yahoo.com>
2026-02-19 00:13:02 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{"Sanjay "} Are you having a nice time ? <br>
+      Hello World.{"Sanjay "} Are you having a nice time
       <Link href="/about">
         About
       </Link>
